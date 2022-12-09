ce06fd58faf097e81a06416a4369d1f93488ee3d
mamoonamjad17 <113450251+mamoonamjad17@users.noreply.github.com>
2022-12-09 13:04:29 +0500

    sign in form and sign uo form made

--- ServerSide/ServerSide/app.js
+++ ServerSide/ServerSide/app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 const expressLayout= require('express-ejs-layouts')
 var productRouter = require('./routes/API/product');
 const userAuth = require('./routes/userAuth')
-//const userRouter = require('./routes/user')
+const userRouter = require('./routes/user')
 const mongoose = require('mongoose');
 
 
@@ -19,20 +19,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(expressLayout);
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(expressLayout);
 
 app.use('/api/products', productRouter);
-app.use('/user', userAuth);
-
-app.get('signin',(req,res)=>{
-  res.render('Login')
-})
-app.get('/home',(req,res)=>{
-  res.render('home')
-})
-//app.use('/', userRouter);
+app.use('/auth', userAuth);
+app.use('/user', userRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ ServerSide/ServerSide/routes/user.js
@@ -0,0 +1,12 @@
+const express= require('express')
+const router = express.Router()
+const userModels = require('../models/user')
+
+router.get('/sign-in',(req,res)=>{
+  res.render('login')
+})
+router.get('/sign-up',(req,res)=>{
+  res.render('signup')
+})  
+
+module.exports = router;
